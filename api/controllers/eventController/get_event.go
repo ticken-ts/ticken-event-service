8ac9c3a2bde5f240a9a815c58a6f602fec8c7401
Javier <javiferreyra97@gmail.com>
2022-10-01 02:26:32 -0300

    Add getEvent body

--- api/controllers/eventController/get_event.go
+++ api/controllers/eventController/get_event.go
@@ -1,9 +1,15 @@
 package eventController
 
 import (
+	"encoding/json"
 	"github.com/gin-gonic/gin"
+	"ticken-event-service/models"
 )
 
 func (controller *EventController) GetEvent(ctx *gin.Context) {
-
+	res, err := json.Marshal(models.Event{})
+	if err != nil {
+		panic("error returning event")
+	}
+	ctx.Data(200, "application/json", res)
 }
