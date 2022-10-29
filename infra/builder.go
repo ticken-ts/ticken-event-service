b5fef0759d94ed0122dc106757c61ffc63107465
facundotorraca <ftorraca@fi.uba.ar>
2022-10-29 02:20:16 -0300

    add devbus_publisher to use as bus publisher during dev

--- infra/builder.go
+++ infra/builder.go
@@ -6,6 +6,7 @@ import (
 	pvtbc "github.com/ticken-ts/ticken-pvtbc-connector"
 	"github.com/ticken-ts/ticken-pvtbc-connector/fabric/peerconnector"
 	"ticken-event-service/config"
+	"ticken-event-service/env"
 	"ticken-event-service/infra/bus"
 	"ticken-event-service/infra/db"
 )
@@ -45,24 +46,6 @@ func (builder *Builder) BuildDb(connString string) Db {
 	return tickenDb
 }
 
-func (builder *Builder) BuildBusPublisher(connString string) BusPublisher {
-	var tickenBus BusPublisher = nil
-
-	switch builder.tickenConfig.Bus.Driver {
-	case config.RabbitMQDriver:
-		tickenBus = bus.NewRabbitMQPublisher()
-	default:
-		panic(fmt.Errorf("bus driver %s not implemented", builder.tickenConfig.Bus.Driver))
-	}
-
-	err := tickenBus.Connect(connString, builder.tickenConfig.Bus.Exchange)
-	if err != nil {
-		panic(err)
-	}
-
-	return tickenBus
-}
-
 func (builder *Builder) BuildEngine() *gin.Engine {
 	return gin.Default()
 }
@@ -81,6 +64,31 @@ func (builder *Builder) BuildPvtbcListener() *pvtbc.Listener {
 		panic(err)
 	}
 	return listener
+}
+
+func (builder *Builder) BuildBusPublisher(connString string) BusPublisher {
+	var busPublisher BusPublisher = nil
+
+	switch builder.tickenConfig.Bus.Driver {
+	case config.RabbitMQDriver:
+		busPublisher = bus.NewRabbitMQPublisher()
+	default:
+		panic(fmt.Errorf("bus driver %s not implemented", builder.tickenConfig.Bus.Driver))
+	}
+
+	// if we are on dev, we are running each service separately.
+	// So there is no need to use a real. For this case, we are going
+	// to use a dev bus that mock all calls
+	if env.TickenEnv.IsDev() {
+		busPublisher = bus.NewTickenDevBusPublisher()
+	}
+
+	err := busPublisher.Connect(connString, builder.tickenConfig.Bus.Exchange)
+	if err != nil {
+		panic(err)
+	}
+
+	return busPublisher
 }
 
 func buildPeerConnector(config config.PvtbcConfig) *peerconnector.PeerConnector {
